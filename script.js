12eea42f3bb8173b05fce5700531f080c84f65b3
Elle H <itsellej@gmail.com>
2018-08-05 16:23:01 +0100

    Amend fadeTo

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $('h1').fadeTo(600, 0);
-    $('.tagline').fadeTo(600, 0);
-    $('.btn-cta-light').fadeTo(600, 0);
-    $('.main-image').fadeTo(600, 0);
-    $('h2').fadeTo(600,0);
+    $('h1').fadeTo(1000, 1);
+    $('.tagline').fadeTo(3000, 1);
+    $('.main-image').fadeTo(5000, 1);
+    $('.btn-cta-light').fadeTo(5000, 1);
+    $('h2').fadeTo(1000, 1);
 
 });
