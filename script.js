71236a7dd0931426e783c9681e532f417b26176c
Elle H <itsellej@gmail.com>
2018-08-05 16:33:38 +0100

    Amend fadeTo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('h1').fadeTo(1000, 1);
     $('.tagline').fadeTo(3000, 1);
-    $('.main-image').fadeTo(5000, 1);
+    $('.main-image').fadeTo(3000, 1);
     $('.btn-cta-light').fadeTo(5000, 1);
     $('h2').fadeTo(1000, 1);
 
