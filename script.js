4175b39fdf1eefb63f846d5aae0e8704ed05d69a
luapuhpuh <ania.nnk@gmail.com>
2018-08-06 15:37:05 +0200

    smooth navigation scroll effect

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-$('document').ready(function(){
-  setInterval(function(){ 
-    $('h1').removeClass("reveal-item")
-  }, 7000);
-});
-
-$('document').ready(function() {
-  setInterval(function(){ 
-    $('h1').addClass("reveal-item--visible")
-  }, 9000);
-
-});
+$(document).ready(function () {
+            $("a.scrollLink").click(function (event) {
+                event.preventDefault();
+                $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 400);
+            });
+        });
