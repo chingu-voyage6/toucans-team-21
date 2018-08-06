a9f786a4f992bb838f35bc3012ae57b817af6c9f
luapuhpuh <ania.nnk@gmail.com>
2018-08-06 16:04:41 +0200

    fadeTo jQuery

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
-            $("a.scrollLink").click(function (event) {
-                event.preventDefault();
-                $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 400);
-            });
+ 
+    $('h1').delay(300).fadeTo(3000, 1);
+    $('p.tagline').delay(400).fadeTo(3000, 1);
+    $('.btn-cta-light').delay(700).fadeTo(3000, 1);
+    $('.main-image').delay(700).fadeTo(4000, 1);
+    $('h2').delay(800).fadeTo(3000, 1);
+    
+    $("a.scrollLink").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 400);
         });
+});
